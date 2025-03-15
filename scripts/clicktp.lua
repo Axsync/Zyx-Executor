5009cbfd199449e0e9446733e82cdd8938db3dbe
axsync <164740698+Axsync@users.noreply.github.com>
2025-03-15 12:09:24 +0100

    Update clicktp.lua

--- scripts/clicktp.lua
+++ scripts/clicktp.lua
@@ -1,21 +1,24 @@
 if _G.clicktp == nil then
-	_G.clicktp = true
-	local plr = game:GetService("Players").LocalPlayer
-	local UserInputService = game:GetService("UserInputService")
-	local mouse = plr:GetMouse()
-	repeat wait() until mouse
-	UserInputService.InputBegan:Connect(function(input, gameProcessed)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 then
-			if _G.clicktp and UserInputService:IsKeyDown(Enum.KeyCode.LeftControl) then
-				plr.Character:MoveTo(Vector3.new(mouse.Hit.x, mouse.Hit.y, mouse.Hit.z)) 
-			end
-		end
-	end)
+    _G.clicktp = true
+    local plr = game:GetService("Players").LocalPlayer
+    local UserInputService = game:GetService("UserInputService")
+    local mouse = plr:GetMouse()
+    repeat wait() until mouse
+
+    game.StarterGui:SetCore("SendNotification", {Title="Zyx"; Text="ClickTP Enabled - Press LCtrl + MB1"; Duration=4;})
+
+    UserInputService.InputBegan:Connect(function(input, gameProcessed)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            if _G.clicktp and UserInputService:IsKeyDown(Enum.KeyCode.LeftControl) then
+                plr.Character:MoveTo(Vector3.new(mouse.Hit.x, mouse.Hit.y, mouse.Hit.z)) 
+            end
+        end
+    end)
 else
-	_G.clicktp = not _G.clicktp
-	if _G.clicktp then
-		game.StarterGui:SetCore("SendNotification", {Title="Zyx"; Text="ClickTP Enabled Press LCtrl + MB1"; Duration=4;})
-	else
-		game.StarterGui:SetCore("SendNotification", {Title="Zyx"; Text="ClickTP Disabled"; Duration=4;})
-	end
+    _G.clicktp = not _G.clicktp
+    if _G.clicktp then
+        game.StarterGui:SetCore("SendNotification", {Title="Zyx"; Text="ClickTP Enabled"; Duration=4;})
+    else
+        game.StarterGui:SetCore("SendNotification", {Title="Zyx"; Text="ClickTP Disabled"; Duration=4;})
+    end
 end
